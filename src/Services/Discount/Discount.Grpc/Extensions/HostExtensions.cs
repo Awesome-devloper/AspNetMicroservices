893756a4d4fcd76655f69d27d1bb37f7134c8703
Awesome-devloper <aref.saas@gmail.com>
2021-09-24 17:15:48 +0330

    add Grpc to micro

---
+++ src/Services/Discount/Discount.Grpc/Extensions/HostExtensions.cs
@@ -0,0 +1,68 @@
+﻿using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
+using Npgsql;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Discount.Grpc.Extensions
+{
+    public static class HostExtensions
+    {
+        public static IHost MigrateDatabase<TContext>(this IHost host, int? retry = 0)
+        {
+            int retryForAvailbility = retry.Value;
+            using (var scope=host.Services.CreateScope())
+            {
+                var services = scope.ServiceProvider;
+                var configuration = services.GetRequiredService<IConfiguration>();
+                var logger = services.GetRequiredService<ILogger<TContext>>();
+
+                try
+                {
+                    logger.LogInformation("Maigration postgrasql database");
+                    using var connection = new NpgsqlConnection(configuration.GetConnectionString("DefaultConnection"));
+                    connection.Open();
+
+                    using var command = new NpgsqlCommand { Connection = connection };
+
+                    command.CommandText = "DROP TABLE IF EXISTS Coupon";
+                    command.ExecuteNonQuery();
+
+
+                    command.CommandText = @"	CREATE TABLE Coupon(ID SERIAL PRIMARY KEY         NOT NULL,
+                                                                        ProductName     VARCHAR(24) NOT NULL,
+                                                                        Description     TEXT,
+		                                                                Amount INT
+	                                                                ); ";
+                    command.ExecuteNonQuery();
+
+
+                    command.CommandText = "INSERT INTO Coupon (ProductName, Description, Amount) VALUES ('IPhone X', 'IPhone Discount', 150);";
+                    command.ExecuteNonQuery();
+
+                    command.CommandText = "INSERT INTO Coupon (ProductName, Description, Amount) VALUES ('Samsung 10', 'Samsung Discount', 100);";
+                    command.ExecuteNonQuery();
+
+                    logger.LogInformation("Maigrated postgrasql database");
+
+                }
+                catch (NpgsqlException ex)
+                {
+                    logger.LogError("An Error occurred while migration the postgresql database");
+
+                    if(retryForAvailbility<50)
+                    {
+                        retryForAvailbility++;
+                        System.Threading.Thread.Sleep(2000);
+                        MigrateDatabase<TContext>(host, retryForAvailbility);
+                    }
+                }
+                return host;
+            }
+        }
+    }
+}
---
+++ src/Services/Discount/Discount.Grpc/Program.cs
@@ -0,0 +1,31 @@
+using Discount.Grpc.Extensions;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Hosting;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Discount.Grpc
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            var Host = CreateHostBuilder(args).Build();
+            Host.MigrateDatabase<Program>();
+            Host.Run();
+            //   CreateHostBuilder(args).Build().Run();
+        }
+
+        // Additional configuration is required to successfully run gRPC on macOS.
+        // For instructions on how to configure Kestrel and gRPC clients on macOS, visit https://go.microsoft.com/fwlink/?linkid=2099682
+        public static IHostBuilder CreateHostBuilder(string[] args) =>
+            Host.CreateDefaultBuilder(args)
+                .ConfigureWebHostDefaults(webBuilder =>
+                {
+                    webBuilder.UseStartup<Startup>();
+                });
+    }
+}
